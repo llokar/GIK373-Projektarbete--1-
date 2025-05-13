119392d1bd0ac2f06adbd9d87fa29209c35537c7
lokar <alouisekarlsson@icloud.com>
2025-05-13 11:02:04 +0200

    Filer

---
+++ script.js
@@ -0,0 +1,7 @@
+const labels = ['Avesta', 'Borlänge', 'Falun', 'Hedemora', 'Ludvika', 'Säter', 'Mora'];
+const datasets = [
+    {
+        label: 'Befolkning 2023',
+        data: [22753, 51735, 59986, 15345, 26402, 11243, 20536]
+    }
+];
