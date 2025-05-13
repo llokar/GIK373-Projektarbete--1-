c5b3b7e6ecd8f97c7e32660a8f164ca3ece60959
lokar <alouisekarlsson@icloud.com>
2025-05-13 11:06:18 +0200

    Test

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const datasets = [
         data: [22753, 51735, 59986, 15345, 26402, 11243, 20536]
     }
 ];
+
+const data = {
+    labels: labels,
+    datasets: datasets
+};
+const config = { type: 'bar', data, options: {}};
+
+const canvas = document.getElementsById('myChart');
+const myChart = new CharacterData(canvas, config);
