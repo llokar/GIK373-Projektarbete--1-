b8267de689b0e96f494403219a3051ae460a7e6a
Lovisa Sandell <h23losan@du.se>
2025-05-14 11:33:41 +0200

    14/5 Graphs

--- script.js
+++ script.js
@@ -1,53 +1,221 @@
-const labels = [
-    'Avesta',
-  'Borlänge',
-  'Falun',
-  'Hedemora',
-  'Ludvika',
-  'Säter',
-  'Mora',
-  'Älvdalen',
-  
-];
+/* Lagförda, antal efter huvudbrott; Grov Kvinnofridskränkning, kön och år */
 
-const datasets = [
-    { 
-        label: 'Befolkning 2024', 
-        data: [22932, 52178, 59818, 15443, 26353, 11271, 20679, 10345],
-        backgroundColor: "rgba(145, 35, 223, 0.4)",
-        borderColor: "rgb(235, 103, 213)",
-        borderWidth: 2,
-        hoverBackgroundColor: "rgba(92, 56, 221, 0.4)",
-    },
-    { 
-        label: 'Befolkning 2024', 
-        data: [22932, 52178, 59818, 15443, 26353, 11271, 20679, 10345],
+const urlSCBLagforda = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0201/LE0201Våld/Tema615";
+
+const querySCBLagforda = {
+    "query": [
+      {
+        "code": "Huvudbrott",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "06"
+          ]
+        }
+      },
+      {
+        "code": "Kon",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "1"
+          ]
+        }
+      },
+      {
+        "code": "ContentsCode",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "0000030T"
+          ]
+        }
+      }
+    ],
+    "response": {
+      "format": "json"
     }
-];
+  };
 
-const config = {
-    type: 'bar',
-    data: { labels: labels, datasets: datasets},
-    options: {}
-};
 
-const canvas = document.getElementById('myChart');
-const myChart = new Chart(canvas, config);
+  const reqeustLagforda = new Request(urlSCBLagforda, {
+    method: 'POST',
+    body: JSON.stringify(querySCBLagforda)
+  });
 
-const config2 = {
-    type: 'line',
-    data: {
-        labels: [2022, 2023],
-        datasets: [
-            {
-            label: "Borlänge", 
-            data: [52178, 51735]
-        },
+  fetch(reqeustLagforda)
+    .then((response) => response.json())
+    .then(printSCBLagfordaChart);
+
+
+function printSCBLagfordaChart(dataSCBLagforda) {
+    console.log(dataSCBLagforda);
+    const yearsSCBLagforda = dataSCBLagforda.data;
+    console.log(yearsSCBLagforda);
+
+    const labels = yearsSCBLagforda.map((year) => year.key[2]);
+    console.log(labels);
+
+    const data = yearsSCBLagforda.map((year) => year.values[0]);
+    console.log(data);
+
+    const datasets = [
         {
-            label: 'Falun',
-            data: [59818, 59986]
-            }
-        ]
-    }
+            label: "Lagförda; Grov Kvinnofridskränkning 2017–2023",
+            data: data,
+        }
+    ];
+
+    const chartLagforda = new Chart(
+        document.getElementById("scbLagforda"),
+        {
+            type: 'bar',
+            data: { labels: labels, datasets: datasets}
+        }
+    );
+
 }
-const overYear = new Chart(document.getElementById("overYear"), config2);
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Personer som blir utsatta för misshandel efter relation till förövare 2015-2016 */
+
+const urlSCButsatta =
+"https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0201/LE0201Våld/Tema66";
+
+const querySCButsatta =
+{
+    "query": [
+      {
+        "code": "ContentsCode",
+        "selection": {
+          "filter": "item",
+          "values": [
+            "000002WI"
+          ]
+        }
+      }
+    ],
+    "response": {
+      "format": "json"
+    }
+  };
+
+  const request = new Request(urlSCButsatta, {
+    method: "POST",
+    body: JSON.stringify(querySCButsatta)
+  });
+
+  fetch(request)
+  .then(response => response.json())
+  .then(printSCButsattaChart);
+
+    function printSCButsattaChart(dataSCButsatta) {
+    console.log(dataSCButsatta);
+    const relation = dataSCButsatta.data;
+    console.log(relation);
+    const gender = dataSCButsatta.data;
+    console.log(gender);
+    const amount = dataSCButsatta.data;
+    console.log(amount);
+    
+    const labels = relation.map(relation => relation.key[0]);
+    console.log(labels);
+    const labels1 = gender.map(gender => gender.key[1]);
+    console.log(labels1);
+
+    const data = amount.map(
+        (amount) => amount.values[0]
+    );
+    console.log(data);
+
+    const datasets = [{
+        label: "Utsatta för misshandel, efter relation till förövare 2015-2016",
+        data: data
+    }];
+
+    const myChart = new Chart(
+        document.getElementById("scbUtsatta"),
+    {
+        type: "bar",
+        data: {labels: labels, datasets: datasets}
+    }
+    );
+}
+ 
+
+
+
+/*Anmäld grov kvinnofridskränkning 1998-2023*/
+
+const urlSCBAnmalda = 'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0201/LE0201Våld/Tema613'
+const querySCBAnmalda = {
+    "query": [],
+    "response": {
+      "format": "json"
+    }
+  };
+  
+  const requestAnmalda = new Request(urlSCBAnmalda, {
+    method: 'POST',
+    body: JSON.stringify(querySCBAnmalda)
+  });
+
+  fetch(requestAnmalda)
+    .then((response) => response.json())
+    .then(printSCBAnmaldaChart);
+   
+
+    function printSCBAnmaldaChart(dataSCBAnmalda) {
+        const yearsSCBAnmalda = dataSCBAnmalda.data;
+       console.log(yearsSCBAnmalda);
+
+       const labelsSCBAnmalda = yearsSCBAnmalda.map((year) => year.key[0]);
+       console.log(labelsSCBAnmalda);
+
+       const dataAnmalda = yearsSCBAnmalda.map((year) => year.values[0]);
+       console.log(dataAnmalda);
+
+       const datasetsSCBAnmalda = [{
+        label: 'Anmäld grov kvinnofridskränkning',
+        data: dataAnmalda,
+        borderColor: "rgb(229, 75, 96)",
+        backgroundColor: "rgba(145, 35, 223, 0.4)"
+       }
+    ];
+    new Chart(document.getElementById('scbAnmalda'), {
+       type: 'line',
+       data: {
+            labels: labelsSCBAnmalda,
+            datasets: datasetsSCBAnmalda
+       } 
+    });
+
+    }
