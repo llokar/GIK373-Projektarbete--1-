013aae1a85b57bdb3e2b111fc017d0c6d6c01a2f
lovisawikberg <wikberglovisa@gmail.com>
2025-05-30 17:29:50 +0200

    lite tafflig animering i line-graphen 30/5

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-/* Graf: Grov kvinnofridskränkning i SVGForeignObjectElement, anmälda vs lagförda */
+/* Graf: Grov kvinnofridskränkning anmälda vs lagförda */
 
 const urlSCBAnmalda = 'https://api.scb.se/OV0104/v1/doris/sv/ssd/START/LE/LE0201/LE0201Våld/Tema613';
 
@@ -118,7 +118,7 @@ Promise.all([
     size = 16; 
   }
 
-  let delayed;
+  let delayed = false;
 
 const chartElement = document.getElementById('scbAnmaldaochLagforda');
 observer.observe(chartElement);
@@ -259,6 +259,42 @@ backgroundColor: "#DE2F2B"
     size = 16; 
   }
 
+  /* Animering av trendlinjen */
+  const totalDuration = 1500;
+  const delayBetweenPoints = totalDuration / dataKvinnofrid.length;
+  const previousY = (ctx) => ctx.index === 0 
+    ? ctx.chart.scales.y.getPixelForValue(100) 
+    : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+  const animation = {
+    x: {
+      type: 'number',
+      easing: 'linear',
+      duration: delayBetweenPoints,
+      from: NaN,
+      delay(ctx) {
+        if (ctx.type !== 'data' || ctx.xStarted) {
+          return 0;
+        }
+        ctx.xStarted = true;
+        return ctx.index * delayBetweenPoints;
+      }
+    },
+    y: {
+      type: 'number',
+      easing: 'linear',
+      duration: delayBetweenPoints,
+      from: previousY,
+      delay(ctx) {
+        if (ctx.type !== 'data' || ctx.yStarted) {
+          return 0;
+        }
+        ctx.yStarted = true;
+        return ctx.index * delayBetweenPoints;
+      }
+    }
+  };
+
+
 new Chart(document.getElementById('scbAnmaldaKvinnofrid'), { 
 
 type: 'line', 
@@ -271,6 +307,8 @@ datasets: datasetsSCBKvinnofrid
 
 }, 
     options: {
+      responsive: true,
+      animation: animation,
        scales:{
          y: {
             
